22a38f1eb7f573628d8944f98e8f32b33b9213be
Saad Farooq <saadfarooq.info@gmail.com>
2023-05-11 20:45:26 +0500

    feat: added course validation using schema

--- app/routes/api/course.js
+++ app/routes/api/course.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { createCourse, deleteCourse, getAllCourses, getCourse, patchCourse } from '../../controllers/courses.js';
 import { authenticateJWT } from '../../middleware/jwtAuthenticate.js';
+import validate from '../../middleware/validation.js';
+import { courseSchema } from '../../schemaValidator/courseSchema.js';
 
 const courseRouter = express.Router();
 
@@ -8,13 +10,13 @@ courseRouter.use(authenticateJWT)
 
 courseRouter
   .route('/')
-  .post(createCourse)
+  .post(validate({ body: courseSchema }), createCourse)
   .get(getAllCourses)
 
 courseRouter
   .route('/:id')
   .delete(deleteCourse)
   .get(getCourse)
-  .patch(patchCourse)
+  .patch(validate({ body: courseSchema }), patchCourse)
 
 export default courseRouter;
---
+++ app/schemaValidator/courseSchema.js
@@ -0,0 +1,11 @@
+export const courseSchema = {
+  type: "object",
+  required: ['name'],
+  properties: {
+    name: {
+      type: 'string',
+      minLength: 4,
+      maxLength: 64,
+    }
+  }
+}
